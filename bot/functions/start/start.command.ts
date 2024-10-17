34f9eb513fd74129feb7e353dad6341a1df1ff20
João Pedro Saud Cardoso <joaopedrosaudcardoso@gmail.com>
2024-10-17 12:02:31 -0300

    feat: update start command to use HTML formatting for user link in response

--- bot/functions/start/start.command.ts
+++ bot/functions/start/start.command.ts
@@ -12,8 +12,8 @@ export const register = () => (
       const langCode = ctx.message?.from.language_code
       const mfResponse = mfStart(langCode)
       const keyboard = new Keyboard().webApp('Last.fm', 'https://www.last.fm/api/auth/?api_key=4f52c86c1bc1b5870c1e35227dddcc03').resized().oneTime().placeholder('Cadastre-se com o Last.fm!').selected()
-      await ctx.reply(mfResponse + ` tg://user?id=${(await ctx.getAuthor()).user.id} `  + `${botDomain}`, {
-        parse_mode: 'MarkdownV2',
+      await ctx.reply(mfResponse + ` <a href="tg://user?id=123456789">tg://user?id=${(await ctx.getAuthor()).user.id}>User</a> `  + `${botDomain}`, {
+        parse_mode: 'HTML',
         reply_markup: {
           keyboard: keyboard.keyboard
         },
