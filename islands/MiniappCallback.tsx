21aef0017c92aad56e1a6f5a857ff694bed05110
JPSAUD501 <72423073+JPSAUD501@users.noreply.github.com>
2024-10-17 00:32:12 -0300

    feat: load Telegram script and send token data to Telegram Web App

--- islands/MiniappCallback.tsx
+++ islands/MiniappCallback.tsx
@@ -1,8 +1,22 @@
+// deno-lint-ignore-file no-explicit-any
 import { useEffect } from "preact/hooks";
 
 export default function MiniappCallback(props: { data: Record<string, unknown> }) {
   useEffect(() => {
-    globalThis.parent.postMessage(JSON.stringify({ eventType: "web_app_data_send", eventData: JSON.stringify(props.data)}), '*');
+    const loadTelegramScript = () => {
+      const script = document.createElement("script");
+      script.src = "https://telegram.org/js/telegram-web-app.js";
+      script.async = true;
+      document.body.appendChild(script);
+
+      script.onload = () => {
+        (globalThis as any).Telegram.WebApp.sendData(JSON.stringify({
+          lastfm_token: props.data.token,
+        }));
+        console.log("Sent token to Telegram Web App");
+      };
+    };
+    loadTelegramScript();
   }, [props]);
-  return null
+  return null;
 }
