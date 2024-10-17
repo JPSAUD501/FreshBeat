fab96ef7eded0cf0e375ffb6ca1616fa7c1b26e8
João Pedro Saud Cardoso <joaopedrosaudcardoso@gmail.com>
2024-10-17 12:05:57 -0300

    feat: fix user link formatting in start command response

--- bot/functions/start/start.command.ts
+++ bot/functions/start/start.command.ts
@@ -12,7 +12,7 @@ export const register = () => (
       const langCode = ctx.message?.from.language_code
       const mfResponse = mfStart(langCode)
       const keyboard = new Keyboard().webApp('Last.fm', 'https://www.last.fm/api/auth/?api_key=4f52c86c1bc1b5870c1e35227dddcc03').resized().oneTime().placeholder('Cadastre-se com o Last.fm!').selected()
-      await ctx.reply(mfResponse + ` <a href="tg://user?id=123456789">tg://user?id=${(await ctx.getAuthor()).user.id}>User</a> `  + `${botDomain}`, {
+      await ctx.reply(mfResponse + ` <a href="tg://user?id=${(await ctx.getAuthor()).user.id}">User</a> `  + `${botDomain}`, {
         parse_mode: 'HTML',
         reply_markup: {
           keyboard: keyboard.keyboard
