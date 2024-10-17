2f9f8ee39340867ad3147235244606b434ad0c2b
JPSAUD501 <72423073+JPSAUD501@users.noreply.github.com>
2024-10-17 00:56:35 -0300

    feat: streamline Telegram token sending by removing script loading from MiniappCallback and adding it to the Webhook component

--- islands/MiniappCallback.tsx
+++ islands/MiniappCallback.tsx
@@ -3,20 +3,9 @@ import { useEffect } from "preact/hooks";
 
 export default function MiniappCallback(props: { data: Record<string, unknown> }) {
   useEffect(() => {
-    const loadTelegramScript = () => {
-      const script = document.createElement("script");
-      script.src = "https://telegram.org/js/telegram-web-app.js";
-      script.async = true;
-      document.body.appendChild(script);
-
-      script.onload = () => {
-        (globalThis as any).Telegram.WebApp.sendData(JSON.stringify({
-          lastfm_token: props.data.token,
-        }));
-        console.log("Sent token to Telegram Web App");
-      };
-    };
-    loadTelegramScript();
-  }, [props]);
+    (globalThis as any).Telegram.WebApp.sendData(JSON.stringify({
+      lastfm_token: props.data.token,
+    }));
+  }, [props.data.token]);
   return null;
 }
--- routes/api/lastfm/callback.tsx
+++ routes/api/lastfm/callback.tsx
@@ -1,21 +1,21 @@
-import { Handlers, PageProps } from "$fresh/server.ts";
+import { PageProps } from "$fresh/server.ts";
+import { Head } from '$fresh/runtime.ts'
 import MiniappCallback from '../../../islands/MiniappCallback.tsx'
-
-export const handler: Handlers = {
-  GET(req, ctx) {
-    const url = new URL(req.url);
-    const token = url.searchParams.get("token");
-    console.log("Token:", token);
-    return ctx.render({ token });
-  },
-};
 
 export default function Webhook(props: PageProps) {
   const token = props.url.searchParams.get("token");
   return (
     <div>
-      <h1>Sending token to Telegram...</h1>
-      <MiniappCallback data={{ token }} />
+      <Head>
+        <script src="https://telegram.org/js/telegram-web-app.js" async></script>
+      </Head>
+      <div class="flex flex-col items-center justify-center">
+        <h1 class="text-4xl font-bold">Last.fm Authorization</h1>
+        <p class="my-4">
+          Please wait while we send your token to Telegram.
+        </p>
+        <MiniappCallback data={{ token }} />
+      </div>
     </div>
   );
 }
