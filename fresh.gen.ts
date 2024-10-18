6c545c1ce7824bdc1c4d6391d69f3dbcb65813fc
JPSAUD501 <72423073+JPSAUD501@users.noreply.github.com>
2024-10-18 20:11:04 -0300

    feat: implement redirect functionality in new API route and update start service URL handling

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,6 +4,7 @@
 
 import * as $_404 from './routes/_404.tsx'
 import * as $_app from './routes/_app.tsx'
+import * as $api_go from './routes/api/go.tsx'
 import * as $api_lastfm_callback from './routes/api/lastfm/callback.tsx'
 import * as $api_telegram_webhook from './routes/api/telegram/webhook.ts'
 import * as $index from './routes/index.tsx'
@@ -15,6 +16,7 @@ const manifest = {
   routes: {
     './routes/_404.tsx': $_404,
     './routes/_app.tsx': $_app,
+    './routes/api/go.tsx': $api_go,
     './routes/api/lastfm/callback.tsx': $api_lastfm_callback,
     './routes/api/telegram/webhook.ts': $api_telegram_webhook,
     './routes/index.tsx': $index,
---
+++ routes/api/go.tsx
@@ -0,0 +1,30 @@
+import type { PageProps } from '$fresh/server.ts'
+import Redirect from '../../islands/Redirect.tsx'
+
+
+
+export default function GotoPage(props: PageProps) {
+  const goTo = new URL(props.url).searchParams.get('to')
+  if (goTo === null) return Response.redirect('/', 302)
+  const url = new URL(goTo)
+  const redirectDelay = 2000
+
+  return (
+    <>
+      <div class='flex flex-col items-center h-screen bg-gray-900 text-center p-4'>
+        <div class='bg-gray-800 shadow-lg rounded-lg p-6 w-full max-w-md mt-4'>
+          <h1 class='text-xl font-bold text-white mb-4'>
+            Redirecionando para "{url.hostname}"
+          </h1>
+          <p class='text-base text-gray-400 mb-4'>
+            Caso não seja redirecionado automaticamente em {redirectDelay / 1000} segundos, clique no botão abaixo.
+          </p>
+          <a href={ url.toString() } class='btn btn-outline btn-sm text-white border-white hover:bg-yellow-600 hover:text-white w-full mb-4 no-animation'>
+            Ir para {url.hostname}
+          </a>
+        </div>
+        <Redirect url={url.toString()} delay={redirectDelay} />
+      </div>
+    </>
+  )
+}
